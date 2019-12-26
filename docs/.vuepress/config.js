44a662437a8e2813c8d3acfe4e230f6f9c32ec44
kscript <1258@live.cn>
2019-12-26 17:31:23 +0800

    打包

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,7 +15,7 @@ const devSidebar = () => {
 module.exports = {
     title: 'IM开发文档',
     description: '使用 vue + ts + 网易云信web sdk开发的一个即时聊天工具',
-    base: '/',
+    base: '/press/',
     themeConfig: {
         nav: [
             { text: '开发', link: '/dev/index' },
